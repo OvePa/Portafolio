fddbe9ec9f0c8f02d7c429208af5e2c4d8d7c0b9
OvePaz <omar.velazquez.paz@gmail.com>
2023-07-31 11:16:00 -0600

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Omar Velazquez")
+    content = """ Chalala """
+    st.info(content)
